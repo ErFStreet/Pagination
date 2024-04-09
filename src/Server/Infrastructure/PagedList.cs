494f25740268bf99724ccd905331b48e9468ba26
Erfan-Edalati <erfannstreet@gmail.com>
2024-04-09 09:22:08 +0330

    Add project files.

---
+++ src/Server/Infrastructure/PagedList.cs
@@ -0,0 +1,37 @@
+﻿namespace Server.Infrastructure;
+
+public class PagedList<T> : object
+{
+    public PagedList(List<T> items, int page, int pageSize, int totalCount) : base()
+    {
+        Items = items;
+        Page = page;
+        PageSize = pageSize;
+        TotalCount = totalCount;
+    }
+
+    public List<T> Items { get; set; }
+
+    public int Page { get; set; }
+
+    public int PageSize { get; set; }
+
+    public int TotalCount { get; set; }
+
+    public bool HasNextPage => Page * PageSize < TotalCount;
+
+    public bool HasPrevPage => Page > 1;
+
+    public int TotlaPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+
+    public List<T> Create()
+    {
+        var result =
+            Items
+                .Skip((Page - 1) * PageSize)
+                .Take(PageSize)
+                .ToList();
+
+        return result;
+    }
+}
---
+++ src/Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using Server.Infrastructure;
+
+namespace Server;
+
+internal class Program
+{
+    static void Main()
+    {
+        var names = new List<string>
+        {
+            "Ali","Reza","Hoseein","Akbar","Mohammad"
+            ,"Hashem","Gholam","Erfan","Roya","Amir","Asie"
+            ,"Mostafa","Mehdi","Mahdi","Hasan","Asghar"
+        };
+
+
+        var result = new PagedList<string>(
+            page: 1, pageSize: 5, totalCount: 15, items: names).Create();
+            
+
+        foreach (var name in result)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
